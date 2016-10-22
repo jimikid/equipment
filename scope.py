38450f1c2fad6ff2024fbf20226f494dcf0c8a33
sbaek_lab <sbaek@aelrayyah-ws.enphaseenergy.com>
2016-10-21 17:52:30 -0700

    now scope is indepedent in class form

--- scope.py
+++ scope.py
@@ -4,44 +4,56 @@ Created on 02/26/2016, @author: sbaek
     - initial release
 """
 
-from data_aq_lib.equipment.file_utils import unique_file
+#from data_aq_lib.equipment.file_utils import unique_file
 import os
 import time
 import pandas as pd
 import data_aq_lib.measurement.measurements as mm   
-import data_aq_lib.analysis.waveforms_functions as wf
-import time
-results2=dict()
+#import data_aq_lib.analysis.waveforms_functions as wf
+import data_aq_lib.analysis.waveform_func as wf
+import visa
+from collections import OrderedDict
+results2=OrderedDict()
 
 class scope:
     """
            __INIT__     
-           __STR__      
-           
-    inputs
-            para=                - type:dict
-            equip=                - type:dict  
-            
+           __STR__
     """    
-    
-    def __init__(self, para, equip):              
-        self.return_str= ''   
-        self.par=para
-        self.eq=equip
+
+    def __init__(self, data_path):
+        self.return_str= ''
+        self.data_path=data_path
         self.results, self.df = pd.DataFrame(), pd.DataFrame()
-        self.flag='up'        
-        
-        try:self.pcu=self.eq['pcu']
-        except:pass
+
+        rm = visa.ResourceManager()
+        list_equip=rm.list_resources()
+        for item in list_equip:
+            print item
+            if 'GPIB' in item:   #check equipment by gpib
+                eq=rm.open_resource(item)
+                eq.write('*IDN?')
+                c=eq.read()
+                print c
+                if 'DSO-X' in c:
+                    #cata='SCOPE'
+                    #print item+' ->  set '+cata
+                    #item.update({cata:eq})
+                    #item[cata].write('*IDN?')
+                    #print item[cata].read()
+                    self.eq=eq
+
+                #else: print ' scope is not configuable'
+        #self.eq= self.item.eq
+
     
     def __str__(self):
         self.return_str= '\n\n -- class scope_measurement --' 
-        self.return_str += '\n data_path : %s  ' %self.par['data_path']
-        self.return_str += '\n source_path : %s  ' %self.par['source_path']
+        self.return_str += '\n data_path : %s  ' %self.data_path
         return self.return_str       
- 
-              
-    def initialize(self, trig='4', ch=(1,2,3,4), reset='Off'):
+
+
+    def initialize(self, trig, ch, reset='Off'):
         print '\n initizlize scope...'
         # Initialize scope here   
         if reset == 'On':            
@@ -56,11 +68,8 @@ class scope:
         if 4 in ch: self.set_probe_attenuation_factor(4,10)    
     
         time.sleep(1)
-    
-        #Caution! auto scale can reset the configuration made previously        
-        self.set_auto_scale() 
-        time.sleep(1)
-        
+        #self.set_auto_scale()   #Caution! auto scale can reset the configuration made previously    b
+
         timescale=5e-3        
         self.set_timebase_scale_factor(timescale)
         
@@ -70,14 +79,8 @@ class scope:
         else:
             self.set_trig(int(trig))
             print ' - set trigger : %s' %trig  
-        time.sleep(1) 
-        #scope.set_trig(4)   
-    
-        
-        #print ' - set BW On ch 1,2,3,4' 
-        print ' - set BW Off' 
-    #    scope.set_channel_BW_limit(1)
-    
+        time.sleep(1)
+
         print ' - set input impedance'     
         #def set_probe(self, channel, impedance):
         if 1 in ch: self.set_probe(1, 'FIFT')
@@ -101,177 +104,129 @@ class scope:
         time.sleep(1)
   
    
-    def get_data(self, chan):
+    def get_data(self, chan, NumPt):
         w=[]        
-        t,v=self.wave(chan, self.par['NumPt']) # minimum 100
+        t,v=self.wave(chan, NumPt) # minimum 100
         w.append(t)
         w.append(v)
         return w    
-        
-    def calibrate(self, list1, lagging_time, channel): 
-            temp=[]
-            for i in range(len(list1[1])):
-                temp.append(list1[1][i])
-            timestep=list1[0][100]-list1[0][99]
-            pts=int(round(lagging_time/timestep))    
-            
-            print ' calibrate Ch %s,  timestep:%.1f ns , pts:%s' %(channel, timestep*1e9, pts) 
-            if lagging_time>0:
-                for i in range(pts,len(list1[0])-pts-1):            
-                    list1[1][i] = temp[i-pts]
-            elif lagging_time<0:
-                for i in range(len(list1[0])-pts-1):
-                    list1[1][i] = temp[1][i+pts]   
-            return pts        
-     
+
                       
-    def DC2AC_measurement(self):  
+    def DC2AC_measurement(self, filename, pts_deg, NumPt, TimeDiv, ch_names):
         waveforms_list=[]
-        self.par['section']=len(self.par['degree'])
-     
         ''' collect data'''
         d=[]
-        
-        for it in range(self.par['section']):         
-            self.par['phase']=self.par['degree'][it]  # lets say phase is the deg that we are in, 60Hz
-            self.par['timedelay']=self.par['phase']/360/60
-            
-            #results, data_file, self.waveforms, mode =self.do_measure(it)
-            results, data_file, self.waveforms, mode = self.get_waveforms(it)
+        for deg in pts_deg:
+            TimeShift=deg/360/60.0
+            results, self.waveforms = self.get_waveforms(filename, deg, NumPt, TimeDiv, TimeShift, ch_names)
             waveforms_list.append(self.waveforms)
-      
-            results.update({'Sec':it+1,'phase':self.par['phase'], 'NumPt':self.par['NumPt'], 'timeDiv':self.par['timeDiv'], 'calib':self.par['calib'], 'freq':results['freq']})    
+            results.update({'deg':deg, 'NumPt':NumPt, 'TimeDiv':TimeDiv,  'freq':results['freq']})
             d.append(results)     
-            self.waveforms.print_rms()
-            self.waveforms.print_avg()
+
         df=pd.DataFrame(d)    
-        df=df.set_index([range(len(df))])    #set index. Without this,  index numbers are all 'zero'    
- 
-        file_name = self.par['data_path']+'/%s_%sW_%sV_DC2AC.csv' % (self.par['data_file_name'], self.par['p_rated'], self.par['SAS_volt'])
+        df=df.set_index([range(len(df))])    #set index. Without this,  index numbers are all 'zero'
+        file_name = self.data_path+'%s_DC2AC_.csv' % filename
         print "\n Data will be stored as %s\n" % file_name
-        df.to_csv(file_name)    
+        df.to_csv(file_name)
           
         return waveforms_list
  
-    def get_waveforms(self, it):
-        para, equip= self.par, self.eq
-        data_file=''        
+    def get_waveforms(self, filename, deg, NumPt, TimeDiv, TimeShift, ch_names):
+        self.NumPt, self.TimeDiv, self.TimeShift, self.ch_names = NumPt, TimeDiv, TimeShift, ch_names
+        w=[]
+
         results=dict()
- 
-        SCOPE=equip['SCOPE']           
-        #ch1 = w[0] ch2 = w[1], ch3 = w[2], ch4 = w[3], which is also a list of time and value
- 
-        w=[]        
-        timedelay, degree, timeDiv, NumPt = para['timedelay'], int(para['phase']), para['timeDiv'], para['NumPt']
-    
+        if ch_names==None:
+            ch_names=['ch1','ch2','ch3','ch4']
+        SCOPE=self.eq
+
         print '\n---------------------------------------------------------------' 
-        print '--- SEC %s, phase shift: %s degree (%.1f us)'  %(str(it+1), degree, para['timedelay']*1e6)       
-        self.set_timebase_delay_factor(timedelay)    
-        self.set_timebase_scale_factor(timeDiv)             
+        print '--- Deg %s, shift %.1f us'  %(str(deg), TimeShift*1e6)
+        self.set_timebase_delay_factor(TimeShift)
+        self.set_timebase_scale_factor(TimeDiv)
            
         time.sleep(1)
         self.set_stop()
     
-        print ' - save data with %s pts' %str(NumPt)   
+        print ' - save data with %s pts' %str(NumPt)
         for i in range(4):
-            w.append(self.get_data(i+1))            
+            w.append(self.get_data(i+1, NumPt))
         SCOPE.write(':RUN')   
-        
-        if True: # if you want to save original waveforms    
-            scope_data=[w[0][0],w[0][1],w[1][1],w[2][1],w[3][1]]                              
-            df=pd.DataFrame({'Time':scope_data[0], para['ch_names'][0]:scope_data[1], para['ch_names'][1]:scope_data[2], para['ch_names'][2]:scope_data[3], para['ch_names'][3]:scope_data[4]})
-            data_fp, data_file = unique_file(para['data_path']+'/%s.csv' %'scope_data')    
-            df.to_csv(data_file)      
-                      
-        pts=[]
-        calib=para['calib']
-        if calib[0]=='ON':
-            print '\n Calib. On'
-            # channel 3 and 4 has 20ns delay
-            for i in range(len(calib[1])):
-                channel=calib[1][i][0]        
-                pts.append(self.calibrate(w[channel-1], calib[1][i][1], channel))
-        else: pts=0
-                
-        #clear up with wrong data due to time shift   
-        if not(pts==0):                
-            for m in range(4):
-                for i in range(max(pts)*2):
-                    w[m][0].pop(0)
-                    w[m][1].pop(0)
-                    w[m][0].pop(len(w[m])-1)
-                    w[m][1].pop(len(w[m])-1)    
-                    
-#       #ch1 = w[0] ch2 = w[1], ch3 = w[2], ch4 = w[3], which is also a list of time and value
-        waveforms=[w[0][0],w[0][1],w[1][1],w[2][1],w[3][1]]  
-            
-        d={'Time':waveforms[0], para['ch_names'][0]:waveforms[1], para['ch_names'][1]:waveforms[2], para['ch_names'][2]:waveforms[3], para['ch_names'][3]:waveforms[4]}
-        df=pd.DataFrame(d)
-        data_fp, data_file = unique_file(para['data_path']+'/%s.csv' % para['data_file_name'])    
-        print "\n Data will be stored in %s" % data_file
-        df.to_csv(data_file)
-  
-        mode=it+1
-        file_name='data'
-        waveforms=wf.waveforms(para, file_name=file_name) 
-        if para['cycle']:
-           VIrms, VIavg, freq= waveforms.conversion(mode=mode, data_file=data_file, cycle=para['cycle'])   
-        else:VIrms, VIavg, freq= waveforms.conversion(mode=mode, data_file=data_file)               
-  
-        results.update(VIrms)
-        results.update(VIavg)
+
+        waveforms=[w[0][0],w[0][1],w[1][1],w[2][1],w[3][1]]
+
+        data_dict=OrderedDict({'t': waveforms[0]})  #put 't' first to put htat in the first colume to work with wave_func.py
+        data_dict.update({ch_names[0]:waveforms[1], ch_names[1]:waveforms[2], ch_names[2]:waveforms[3], ch_names[3]:waveforms[4]})
+        df=pd.DataFrame(data_dict)
+
+        file_name=self.data_path+'%s_scope_%s.csv' % (filename, str(deg))
+        print "\n save as %s" % file_name
+        df.to_csv(file_name)
+
+        waves=wf.waveforms(df=df,  filepath=self.data_path)
+        rms=waves.get_rms()
+        avg=waves.get_avg()
+        pkpk=waves.get_pkpk()
+        zeros= waves.get_zero_crossing(waves.get_labels()[2])
+        freq = waves.get_freq(waves.get_labels()[2], zeros)
+        print ' %.1f kHz' %(freq/1000)
+        waves.plot_all()
+
+        rms_dict={ch_names[0]+'_rms':rms[0], ch_names[1]+'_rms':rms[1],
+                    ch_names[2]+'_rms':rms[2], ch_names[3]+'_rms':rms[3]}
+        avg_cit={ch_names[0]+'_avg':avg[0] , ch_names[1]+'_avg':avg[1],
+                    ch_names[2]+'_avg':avg[2] , ch_names[3]+'_avg':avg[3] }
+
+        results.update(rms_dict)
+        results.update(avg_cit)
         results.update({'freq':freq})
-    
-        return results, data_file, waveforms, mode    
-   
-     
+        return results, waveforms
+
     def reset(self):
-        self.eq['SCOPE'].write('*RST')
+        self.eq.write('*RST')
     
     def set_probe_attenuation_factor(self, channel, attenuation):
-        self.eq['SCOPE'].write(':CHAN%s:PROBE %s' % (channel, attenuation))    
+        self.eq.write(':CHAN%s:PROBE %s' % (channel, attenuation))    
     
     def set_auto_scale(self):
         print ' - autoscale' 
-        self.eq['SCOPE'].write(':AUToscale')
+        self.eq.write(':AUToscale')
  
     def set_probe(self, channel, impedance):       
-        self.eq['SCOPE'].write(':CHAN%s:IMP %s' % (channel, impedance))
+        self.eq.write(':CHAN%s:IMP %s' % (channel, impedance))
     #    
     def set_channel_UnitAmp(self, channel):
-        self.eq['SCOPE'].write(':CHAN%s:UNIT AMP' % channel)
+        self.eq.write(':CHAN%s:UNIT AMP' % channel)
     #
     def set_timebase_scale_factor(self, timebase_scale):
         print ' - set time scale %s ms' %(timebase_scale*1000) 
-        self.eq['SCOPE'].write(':TIM:SCAL %s' % timebase_scale)
+        self.eq.write(':TIM:SCAL %s' % timebase_scale)
     
     def set_trig(self, channel):
         #<source> ::= {CHANnel<n> | EXTernal | LINE | WGEN} for the DSO models
         if channel=='EXT':
-            self.eq['SCOPE'].write(':TRIG:EDGE:SOUR EXT')
+            self.eq.write(':TRIG:EDGE:SOUR EXT')
         else:
-            self.eq['SCOPE'].write(':TRIG:EDGE:SOUR CHAN%s'  % channel)
+            self.eq.write(':TRIG:EDGE:SOUR CHAN%s'  % str(channel))
        
     def display_channel(self, channel):
-        self.eq['SCOPE'].write(':CHAN%s:DISP ON' % channel)
+        self.eq.write(':CHAN%s:DISP ON' % channel)
     #    
     def set_channel_Invert(self, channel):
-        self.eq['SCOPE'].write(':CHAN%s:INV ON' % channel)       
+        self.eq.write(':CHAN%s:INV ON' % channel)       
     
     def set_stop(self):
-        self.eq['SCOPE'].write(':STOP')
-    #def set_run(self):
-    #    self._write(':RUN')    
-    #
+        self.eq.write(':STOP')
+
     def set_offset_all(self, offset):
-        self.eq['SCOPE'].write(':CHAN1:OFFS %s' % offset)
-        self.eq['SCOPE'].write(':CHAN2:OFFS %s' % offset)
-        self.eq['SCOPE'].write(':CHAN3:OFFS %s' % offset)
-        self.eq['SCOPE'].write(':CHAN4:OFFS %s' % offset)
+        self.eq.write(':CHAN1:OFFS %s' % offset)
+        self.eq.write(':CHAN2:OFFS %s' % offset)
+        self.eq.write(':CHAN3:OFFS %s' % offset)
+        self.eq.write(':CHAN4:OFFS %s' % offset)
 
 
     def set_timebase_delay_factor(self, timebase_delay):
-        self.eq['SCOPE'].write(':TIM:DEL %s' % timebase_delay)
+        self.eq.write(':TIM:DEL %s' % timebase_delay)
         
     def wave(self, channel, num_of_points='MAX', filename=None):
         """Record one channel of data from oscilloscope."""
@@ -281,41 +236,27 @@ class scope:
         # self._write('WAV:BYTEORDER MSBF')
 
         # self._write('WAV:UNSIGNED 1') # Return unsigned numbers
-        self.eq['SCOPE'].write('WAV:FORMAT ASC')
+        self.eq.write('WAV:FORMAT ASC')
 
         # It appears that the oscilloscope returns 1 less than requested, therefore bump up the request by 1.
         if type(num_of_points) == int:
             num_of_points += 1
-
         if channel == 'MATH':
             #print 'WAV:SOURCE MATH'
-            self.eq['SCOPE'].write('WAV:SOURCE MATH')
+            self.eq.write('WAV:SOURCE MATH')
         else:
             #print 'WAV:SOURCE CHANNEL%s' % channel
-            self.eq['SCOPE'].write('WAV:SOURCE CHANNEL%s' % channel)
-        self.eq['SCOPE'].write('WAV:POINTS %s' % num_of_points)
+            self.eq.write('WAV:SOURCE CHANNEL%s' % channel)
+        self.eq.write('WAV:POINTS %s' % num_of_points)
 
-        data = self.eq['SCOPE'].ask('WAV:DATA?')
+        data = self.eq.ask('WAV:DATA?')
         ydata = data[10:]
         ydata = map(float, ydata.split(','))
 
-        wave_preamble = self.eq['SCOPE'].ask('WAV:PRE?')
+        wave_preamble = self.eq.ask('WAV:PRE?')
         preamble = [float(v) for v in wave_preamble.split(',')]
-
         format_type, ttype, points, count, xincrement, xorigin, xreference, yincrement, yorigin, yreference = preamble
         xdata = [(x - xreference) * xincrement + xorigin for x in range(int(points))]
-
         #self.restore_timeout()
 
-        if filename:
-            # fp, filename = unique_file(filename)
-            filename = os.path.normpath(filename)
-            fp = open(filename, 'w')
-            # print filename
-            for x, y in zip(xdata, ydata):
-                fp.write('%e, %e\n' % (x, y))
-            fp.close()
-            return filename
-
         return xdata, ydata
-
